2dc89ee4e9a37a9a4ab781dc75395f61dd53da34
Himmat Magar <himmatmagar007@gmail.com>
2026-06-06 13:54:55 +0545

    using oop for download data

--- data/main.py
+++ data/main.py
@@ -1,9 +1,21 @@
 import urllib.request as request
 
-def download_data():
-      filename, header = request.urlretrieve(
-            url="https://github.com/master-temp/movie-rec/raw/refs/heads/main/movies_metadata.csv",
-            filename="movies.csv"
-      )
 
-download_data()
+class DownloadData:
+      
+      def __init__(self):
+            self.url = "https://github.com/master-temp/movie-rec/raw/refs/heads/main/movies_metadata.csv"
+      
+      
+      def download_data(self):
+            filename, header = request.urlretrieve(
+                  url = self.url,
+                  filename = "movies_data.csv"
+            )
+
+            print(f"{filename} downloaded with following info: \n{header}")
+
+
+if __name__ == "__main__":
+      data = DownloadData()
+      data.download_data()
